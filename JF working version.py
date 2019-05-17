f02cdff629d27bd57e17dece9e48beb5afab1ff7
Joe Ford <joseph2.ford@live.uwe.ac.uk>
2019-05-17 12:07:57 +0100

    Added JF working version
    Server storage to pick up from different laptop

---
+++ JF working version.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+data = pd.read_csv("dorothea_smaller.csv")
+y-value = data["y.train"]
+
+data.columns
+
+for i = 1:10000
+data.iloc[:,i]
+
